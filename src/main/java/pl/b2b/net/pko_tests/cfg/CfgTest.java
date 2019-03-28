bad4083f56e2e2cf46771a4c089e22ffba6a3836
dorotaglazek <dorota.glazek@gmail.com>
2019-03-28 14:10:50 +0100

    zmiana parametrów w CfgTest

--- src/main/java/pl/b2b/net/pko_tests/cfg/CfgTest.java
+++ src/main/java/pl/b2b/net/pko_tests/cfg/CfgTest.java
@@ -13,24 +13,24 @@ public class CfgTest {
     public static String excelFilePath;
 
     @Test(priority = 1)
-    @Parameters({"driverPath","url","excelPath"})
-    @TestFactoryMethod(value = "start", description = "inicjalizacja srodowiska testowego", group = "konfiguracja")
-    public void setUp(String driverPath,String url,String excelPath) {
-        websiteAddress = url;
-        excelFilePath = excelPath;
+    @Parameters({"driverPath"})
+    @TestFactoryMethod(value = "Start", description = "Inicjalizacja środowiska testowego", group = "konfiguracja")
+    public void setUp(String driverPath) {
         WebDriverCfg.Initialize(driverPath);
         Assert.assertNotNull(WebDriverCfg.getWebDriverInstance());
     }
     @Test(priority = 2)
-    @TestFactoryMethod(value = "otworz strone", description = "przejdz do strony, ktorej adres podany zostal w parametrze", group = "konfiguracja")
-    @Parameters({"url"})
-    public void openSite(String url) {
-        WebDriverCfg.getWebDriverInstance().get(url);
+    @Parameters({"url","excelPath"})
+    @TestFactoryMethod(value = "Otwórz stronę i podaj lokalizację pliku z danymi", description = "Otwórz stronę i podaj lokalizację pliku excel z danymi", group = "konfiguracja")
+    public void openSite(String url, String excelPath) {
+        websiteAddress = url;
+        excelFilePath = excelPath;
+        WebDriverCfg.getWebDriverInstance().get(websiteAddress);
         Assert.assertNotNull(WebDriverCfg.getWebDriverInstance().getCurrentUrl());
     }
 
     @Test(priority = 3)
-    @TestFactoryMethod(value = "stop", description = "zakoncz test", group = "konfiguracja")
+    @TestFactoryMethod(value = "Stop", description = "Zakoncz test", group = "konfiguracja")
     public void tearUp() {
         WebDriverCfg.quitWebDriverInstance();
         Assert.assertNull(WebDriverCfg.getWebDriverInstance());
